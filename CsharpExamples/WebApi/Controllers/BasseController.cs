859f6a559bc766e006afe493bfb30835ed3ddef1
Oleksandr <olkly85@gmail.com>
2015-09-03 22:45:31 +0200

    Ihttpactionresult
    generic implementation of httpresult

--- CsharpExamples/WebApi/Controllers/BasseController.cs
+++ CsharpExamples/WebApi/Controllers/BasseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -11,57 +13,8 @@ namespace WebApi.Controllers
     public abstract class BasseController : ApiController
     {
 
-        protected IDataService iDataService;
-
-       
-        [Route("api/getallasync")]
-        public virtual  async Task<IHttpActionResult> GetAllAsync()
-        {
-            return Ok(await iDataService.GetAllAsync());
-        }
-        [Route("api/getallsync")]
-        public virtual IHttpActionResult GetAllSync()
-        {
-            return Ok(iDataService.GetAllSync());
-        }
-
-
-        public async Task<IHttpActionResult> Get(int Id)
-        {
-
-            return Ok( await iDataService.Get(Id));
-        }
-        public IHttpActionResult Post([FromBody]Employee employee)
-        {
-            iDataService.Add(employee);
-            return Ok();
-        }
-        public IHttpActionResult Put([FromBody]Employee employee, int Id)
-        {
-            try
-            {
-                iDataService.Update(employee, Id);
-                return Ok();
-            }
-            catch
-            {
-                return NotFound();
-            }
-        }
-        public  IHttpActionResult Delete(int id)
-        {
-            try
-            {
-                iDataService.Delete(id);
-                return Ok();
-            }
-            catch
-            {
-                return NotFound();
-            }
-
-        }
-
+      
 
     }
+   
 }
--- CsharpExamples/WebApi/Controllers/EmployeeController.cs
+++ CsharpExamples/WebApi/Controllers/EmployeeController.cs
@@ -11,6 +11,8 @@ using System.Web.Http.OData.Query;
 using System.Web.Script.Serialization;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Net.Http.Formatting;
 
 
 
@@ -21,16 +23,117 @@ namespace WebApi.Controllers
      /// </summary>
     [ClaimsAuthorize("role", "admin")]
    
-    public class EmployeeController :  BasseController
+    public class EmployeeController : ApiController
     {
-        
+
+        IDataService iDataService;
         public EmployeeController(IDataService iDataService)
         {
-            base.iDataService = iDataService;
+            this.iDataService = iDataService;
         }
 
+
        
+        public virtual async Task<IHttpActionResult> Get()
+        {
+            
+            return new HttpResult(await iDataService.GetAllAsync(), Request, HttpStatusCode.OK);
+        }
+        [Route("api/gettest")]
+        public IHttpActionResult GetTest()
+        {
+
+            return new HttpResult(HttpStatusCode.Unauthorized);
+        }
+        
+
+        [Route("api/getallasync")]
+        public virtual async Task<IHttpActionResult> GetAllAsync()
+        {
+            return new HttpResult(await iDataService.GetAllAsync(), Request, HttpStatusCode.OK);
+        }
+        [Route("api/getallsync")]
+        public virtual IHttpActionResult GetAllSync()
+        {
+            //return Ok(iDataService.GetAllSync());
+            return new HttpResult(iDataService.GetAllSync(), Request, HttpStatusCode.OK);
+        }
+
+
+        public async Task<IHttpActionResult> Get(int Id)
+        {
+            return new HttpResult(await iDataService.Get(Id), Request, HttpStatusCode.OK);
+            //return Ok( await iDataService.Get(Id));
+
+        }
+        public IHttpActionResult Post([FromBody]Employee employee)
+        {
+            iDataService.Add(employee);
+            return Ok();
+        }
+        public IHttpActionResult Put([FromBody]Employee employee, int Id)
+        {
+            try
+            {
+                iDataService.Update(employee, Id);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                iDataService.Delete(id);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+        }
        
+
+    }
+    public class HttpResult : IHttpActionResult
+    {
+        private ObjectContent content;
+        private HttpStatusCode code;
+        private HttpRequestMessage request;
+        public HttpResult(object content, HttpRequestMessage request, HttpStatusCode code)
+        {
+            this.request = request;
+            this.code = code;
+            this.content = new ObjectContent(content.GetType(), content, new JsonMediaTypeFormatter());
+        }
+        public HttpResult( HttpRequestMessage request, HttpStatusCode code)
+        {
+            this.request = request;
+            this.code = code;
+           
+        }
+
+        public HttpResult( HttpStatusCode code)
+        {
+            this.code = code;
+
+        }
+
+        public Task<System.Net.Http.HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content =content,
+                StatusCode = code,
+                RequestMessage = request
+            };
+
+            return Task.FromResult(response);
+        }
 
     }
 }
